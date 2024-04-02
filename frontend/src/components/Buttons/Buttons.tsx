917463fb341e9ac9d027ac24f849568c467a4b06
Biplove Gautam <biplovegautam123@gmail.com>
2024-04-02 09:24:18 +0545

    tailwind

---
+++ frontend/src/components/Buttons/Buttons.tsx
@@ -0,0 +1,15 @@
+export const PrimaryButton = () => {
+  return <div>PrimaryButton</div>;
+};
+
+export const SecondaryButton = () => {
+  return <div>SecondaryButton</div>;
+};
+
+export const TertiaryButton = () => {
+  return <div>TertiaryButton</div>;
+};
+
+export const AddToCartButton = () => {
+  return <div>AddToCartButton</div>;
+};
