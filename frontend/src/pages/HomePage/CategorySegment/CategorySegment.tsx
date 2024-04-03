148fd326acd5ea14856430eb3c3a7771bac0215a
Biplove Gautam <biplovegautam123@gmail.com>
2024-04-03 10:47:44 +0545

    propertion fixed

--- frontend/src/pages/HomePage/CategorySegment/CategorySegment.tsx
+++ frontend/src/pages/HomePage/CategorySegment/CategorySegment.tsx
@@ -8,7 +8,7 @@ const CategorySegment = () => {
     <>
       <section className={` bg-bgLightBlue px-10vw py-5vh ${paddingForPage}`}>
         <SortByPanel />
-        <h1 className=" text-primaryBlue text-3xl font-bold w-100% sm:w-70% xmd:w-50% ">
+        <h1 className=" text-primaryBlue text-3xl 3xl:text-5xl font-bold w-100% sm:w-70% xmd:w-70% mb-5%">
           WE HAVE SOMETHING FOR EVERYONE
         </h1>
         <div id="container" className="grid ">
