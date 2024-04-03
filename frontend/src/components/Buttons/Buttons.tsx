aa5c88d27a0f5de24a5992adb98f6653b6fab77a
Biplove Gautam <biplovegautam123@gmail.com>
2024-04-03 07:27:52 +0545

    Category Segment almost done for desktop version

--- frontend/src/components/Buttons/Buttons.tsx
+++ frontend/src/components/Buttons/Buttons.tsx
@@ -13,7 +13,7 @@ export const TertiaryButton = () => {
 export const AddToCartButton = () => {
   return (
     <>
-      <button className=" bg-primaryBlue text-bodybg py-2 px-5 rounded-md flex gap-2">
+      <button className=" bg-primaryBlue text-bodybg text-sm py-2 px-5 rounded-md flex gap-2">
         Add to cart
         <img src="/icons/whiteCartMedium.svg" alt="whiteCart" />
       </button>
