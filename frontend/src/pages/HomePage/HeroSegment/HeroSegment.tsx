55ddeb4c786df458b148eb7ca8b4f2fdeac4cf19
Your Name <sunilcrestha9@gmai.com>
2024-04-04 08:24:07 -0700

    modified hero

--- frontend/src/pages/HomePage/HeroSegment/HeroSegment.tsx
+++ frontend/src/pages/HomePage/HeroSegment/HeroSegment.tsx
@@ -1,18 +1,26 @@
 import { paddingForPage } from "../../../defineSize"
 export default function HeroSegment() {
+  // sideWhite Button Style 
+  const sideWhiteButtonStyle ="bg-bgLightBlue w-8 aspect-square rounded-full border-4 border-white sm:bg-bodybg sm:w-12 sm:h-10 sm:border-none";
+
+
   return (
     <div className={`  bg-bodybg  ${paddingForPage}`}>
       {/* main grid */}
       <div className=" grid sm:grid-cols-5  py-8 ">
         {/* left image */}
-        <div className="col-span-4 sm:col-span-2 mt-5 sm:mt-0  max-h-90vw">
+        <div className="col-span-4 sm:col-span-2 mt-5 sm:mt-0  max-h-90vw relative">
             <img className=" w-full h-full object-cover object-top" src="./assets/image1.png " alt="" />
+            <div className=" absolute top-16 flex flex-col gap-30vh sm:gap-40vh -right-3 sm:-right-4">
+              <div className={`${sideWhiteButtonStyle}`}></div>
+              <div className={`${sideWhiteButtonStyle}`}></div>
+            </div>
         </div>
         {/* right contents */}
         <div className=" col-span-3 grid grid-rows-2 sm:ml-5">
           <div className=" grid grid-flow-col  sm:grid-rows-2 order-last sm:order-none">
             <div className="">
-              <p className="text-4xl absolute top-14 sm:relative sm:top-0 sm:text-4xl xl:text-5xl text-primaryBlue font-semibold  xl:leading-normal">CRAFTED FOR THE <span className=" text-bodybg sm:text-primaryBlue">DISCERNING INDIVIDUAL</span></p>
+              <p className="text-4xl absolute top-14 sm:relative sm:top-0 sm:text-4xl xl:text-5xl text-primaryBlue font-extrabold  xl:leading-normal">CRAFTED FOR THE <span className=" text-bodybg sm:text-primaryBlue">DISCERNING INDIVIDUAL</span></p>
             </div>
             <div className="">
               <p className="xmd:text-xl xl:text-2xl text-primaryBlue opacity-70">"Experience the ultimate in luxury and comfort with our handcrafted clothing, tailored to your unique measurements and preferences."</p>
