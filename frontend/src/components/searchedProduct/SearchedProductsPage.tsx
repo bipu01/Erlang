8173b5475ed524171493be685d30a707d5587f27
Your Name <sunilcrestha9@gmai.com>
2024-04-23 08:04:37 -0700

    search some changes

--- frontend/src/components/searchedProduct/SearchedProductsPage.tsx
+++ frontend/src/components/searchedProduct/SearchedProductsPage.tsx
@@ -15,26 +15,39 @@ export default function SearchedProductsPage() {
         }
     }, [location.search]);
 
+    const renderStarRating = (starCount: number) => {
+        const stars = [];
+        for (let i = 0; i < 5; i++) {
+            stars.push(
+                <span key={i} className={i < starCount ? 'text-yellow-500 text-2xl' : 'text-2xl'}>&#9733;</span>
+            );
+        }
+        return stars;
+    };
+    
+
     return (
         <div className={`${paddingForPage}`}>
             <div className="mt-5">
                 <h1 className='text-4xl text-center font-medium font-serif'>Searched Items</h1>
             </div>
-            <div className=" my-4">
-                <p className="text-xl text-black">{searchedProducts.length} items found for " {searchTerm} "</p>
+            <div className="my-4">
+                <p className="text-xl text-black">{searchedProducts.length} items found for "{searchTerm}"</p>
             </div>
-            <div className="" >
+            <div>
                 {searchedProducts.length > 0 ? (
-                    <div className="flex flex-wrap justify-between rounded-lg" >
+                    <div className="flex flex-wrap justify-between rounded-lg">
                         {searchedProducts.map((product, index) => (
                             <div className="bg-gray-200 rounded-lg" key={index}>
                                 <div className="flex flex-col">
-                                    <div className="" >
+                                    <div>
                                         <img className='w-60 h-80 object-cover rounded-md' src={product.image.image1} alt="" />
                                     </div>
-                                    <div className=" w-60">
+                                    <div className="w-60">
                                         <p className='text-lg font-medium text-center py-2'>{product.name}</p>
-
+                                        <div className="text-center">
+                                        {renderStarRating(product.rating.rate) }
+                                        </div>
                                     </div>
                                 </div>
                             </div>
