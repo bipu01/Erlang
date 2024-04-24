05b541e06ca03f065382f0c008512148312e123e
Biplove Gautam <biplovegautam123@gmail.com>
2024-04-24 12:09:07 +0545

    cart layout fixed

--- frontend/src/pages/CartPage/Cart.tsx
+++ frontend/src/pages/CartPage/Cart.tsx
@@ -1,18 +1,22 @@
+import { useState } from "react";
 import ScrollToTop from "../../Functions/ScrollToTop";
+import Star from "../../SVG/Star";
 import { cartItems } from "./cartCluster";
+import { BuyNowBtn } from "../../components/Buttons/Buttons";
 
 export default function Cart() {
+  const [num, setNum] = useState(0);
   return (
     <div className="">
       <ScrollToTop />
-      <div className="">
+      <div className="flex flex-col gap-4">
         {cartItems.map((item, index) => (
           <div
             key={index}
-            className="w-full h-full grid grid-cols-4 sm:grid-cols-5 bg-bodybg rounded-lg   my-2"
+            className="relative w-full h-full grid grid-cols-4 sm:grid-cols-5 bg-bodybg rounded-lg"
           >
             <div className="col-span-1 p-1 sm:p-2">
-              <div className=" w-full aspect-square rounded-md overflow-hidden">
+              <div className=" w-full h-28 sm:h-10rem rounded-md overflow-hidden">
                 <img
                   className=" w-full h-full object-cover object-top"
                   src={`${item.image}`}
@@ -22,64 +26,50 @@ export default function Cart() {
             </div>
             <div className=" col-span-3 sm:col-span-4 flex flex-col gap-1 pl-5 p-1  sm:gap-3 sm:p-2">
               <div className="">
-                <p className=" text-xs sm:text-xl xmd:text-2xl text-primaryBlue font-semibold">
+                <p className=" text-xs sm:text-xl  text-primaryBlue font-semibold">
                   {item.name}
                 </p>
               </div>
 
-              <div className="">
-                <div id="stars" className="flex gap-2 items-baseline">
-                  <div id="stars" className="flex">
-                    <img
-                      src="/icons/filledStar.svg"
-                      alt=""
-                      className=" sm:h-6"
-                    />
-                    <img
-                      src="/icons/filledStar.svg"
-                      alt=""
-                      className=" sm:h-6"
-                    />
-                    <img
-                      src="/icons/filledStar.svg"
-                      alt=""
-                      className=" sm:h-6"
-                    />
-                    <img
-                      src="/icons/filledStar.svg"
-                      alt=""
-                      className=" sm:h-6"
-                    />
-                    <img
-                      src="/icons/unfilledStar.svg"
-                      alt=""
-                      className=" sm:h-6"
-                    />
-                  </div>
+              <div id="stars" className="flex gap-2 items-baseline mt-2">
+                <div id="stars" className="flex gap-2">
+                  <Star height={20} width={20} />
+                  <Star height={20} width={20} />
+                  <Star height={20} width={20} />
+                  <Star height={20} width={20} />
+                  <Star height={20} width={20} fillColor="transparent" />
                 </div>
-              </div>
-              <div className="flex  items-center gap-4 ">
-                <div className="">
-                  <p className="text-xs sm:text-xl xmd:text-2xl text-primaryBlue font-semibold">
-                    Color
-                  </p>
-                </div>
-                <div
-                  className={` h-4 w-4 sm:h-6 sm:w-6 rounded-full bg-${item.color} border-2 border-yellow-200`}
-                ></div>
               </div>
 
-              <div className=" grid grid-cols-3 gap-3 sm:grid-cols-4 sm:gap-8 items-center">
+              <div className="absolute bottom-2 grid grid-cols-3 gap-3 sm:grid-cols-3 sm:gap-8 items-baseline">
                 {/* quantity increase and decrease */}
-                <div className=" flex justify-around">
-                  <button className=" font-extrabold sm:text-2xl">-</button>
-                  <p className="text-xs sm:text-base xmd:text-xl flex items-center rounded-md border-2 px-3 bg-bgLightBlue">
-                    {item.quantity}
+                <div className=" flex gap-6 items-center justify-around">
+                  <button
+                    className=" font-bold sm:text-3xl"
+                    onClick={() => {
+                      if (num >= 0) {
+                        setNum(num - 1);
+                      }
+                    }}
+                  >
+                    -
+                  </button>
+                  <p className="text-xs sm:text-base xmd:text-xl flex items-center rounded-md border-2   px-4 py-1 bg-bgLightBlue">
+                    {num}
                   </p>
-                  <button className=" font-extrabold sm:text-2xl">+</button>
+                  <button
+                    className=" font-semibold sm:text-3xl"
+                    onClick={() => {
+                      if (num <= 20) {
+                        setNum(num + 1);
+                      }
+                    }}
+                  >
+                    +
+                  </button>
                 </div>
                 {/* item original and discount price */}
-                <div className=" sm:col-span-2 text-xs sm:text-base xmd:text-xl flex text-primaryBlue font-semibold justify-center">
+                <div className=" sm:col-span-1 text-xs sm:text-base xmd:text-xl flex text-primaryBlue font-semibold justify-center">
                   <p>NPR.</p>
                   <div className="flex gap-1 sm:gap-2">
                     <p className=" line-through opacity-65">{item.orgPrice}</p>
@@ -87,9 +77,7 @@ export default function Cart() {
                   </div>
                 </div>
                 <div className="">
-                  <button className=" bg-primaryBlue text-white  rounded-md text-xs sm:text-base xmd:text-xl py-1 sm:py-2 px-3 sm:px-5">
-                    Buy now
-                  </button>
+                  <BuyNowBtn text="Buy now" darkBg={true} primary={true} />
                 </div>
               </div>
             </div>
